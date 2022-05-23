fb6dddfcf65ab2cebbeb4b4102ad21d7b9903420
Cesar Pulles <cesarrene.pulles@philips.com>
2022-05-23 23:38:27 +0200

    Removed old code

--- AssetConverter/ConverterState.cpp
+++
@@ -1,7 +0,0 @@
-
-#include "ConverterState.h"
-
-std::filesystem::path ConverterState::ConvertToExportRelative(const std::filesystem::path& path) const
-{
-	return path.lexically_proximate(exportPath);
-}
--- C3DEngine/core/Math.h
+++
@@ -1,8 +0,0 @@
-
-#pragma once
-#include <glm/glm.hpp>
-
-namespace C3D::Math
-{
-	uint32_t PreviousPow2(uint32_t number);
-}
