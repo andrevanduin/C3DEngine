dc84b3f38a76f2ea59329b69d64d30fe02c4cb99
Cesar Pulles <cesarrene.pulles@philips.com>
2022-05-27 21:58:51 +0200

    Added check for allocation count by pressing 'm' in testenv

--- testenv/src/game.cpp
+++ testenv/src/game.cpp
@@ -1,11 +1,25 @@
 
 #include "game.h"
 
+#include <core/memory.h>
+#include <services/services.h>
+
+#include "core/logger.h"
+
 TestEnv::TestEnv(const C3D::ApplicationConfig& config)
 	: Application(config) {}
 
 void TestEnv::OnUpdate(f64 deltaTime)
 {
+	static u64 allocCount = 0;
+	const u64 prevAllocCount = allocCount;
+	allocCount = C3D::Memory::GetAllocCount();
+
+
+	if (INPUT.IsKeyUp(C3D::KeyM) && INPUT.WasKeyDown('m'))
+	{
+		C3D::Logger::Debug("Allocations: {} of which {} happened this frame", allocCount, allocCount - prevAllocCount);
+	}
 }
 
 void TestEnv::OnRender(f64 deltaTime)
