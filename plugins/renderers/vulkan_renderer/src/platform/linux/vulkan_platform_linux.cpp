a5be860cd4d1bc53f0ded39dcd06839b6073aa6c
Cesar Pulles <cesarpulles@gmail.com>
2023-10-15 19:18:54 +0200

    Adde vulkan_platform_linux implementation

---
+++ plugins/renderers/vulkan_renderer/src/platform/linux/vulkan_platform_linux.cpp
@@ -0,0 +1,47 @@
+
+#include "core/defines.h"
+
+#ifdef C3D_PLATFORM_LINUX
+#include <xcb/xcb.h>
+
+#include "vulkan_platform_linux.h"
+
+// For surface creation
+#define VK_USE_PLATFORM_XCB_KHR
+#include <platform/platform.h>
+#include <systems/system_manager.h>
+#include <vulkan/vulkan.h>
+
+#include "vulkan_types.h"
+#include "vulkan_utils.h"
+
+namespace C3D::VulkanPlatform
+{
+    bool CreateSurface(const SystemManager* pSystemsManager, VulkanContext& context)
+    {
+        const LinuxHandleInfo& handle = pSystemsManager->GetSystem<C3D::Platform>(C3D::SystemType::PlatformSystemType).GetHandleInfo();
+
+        VkXcbSurfaceCreateInfoKHR createInfo = { VK_STRUCTURE_TYPE_WIN32_SURFACE_CREATE_INFO_KHR };
+        createInfo.hinstance                 = handle.connection;
+        createInfo.hwnd                      = handle.window;
+
+        auto result = vkCreateXcbSurfaceKHR(context.instance, &createInfo, context.allocator, &context.surface);
+        if (!VulkanUtils::IsSuccess(result))
+        {
+            Logger::Error("[VULKAN_PLATFORM] - CreateSurface() - vkCreateXcbSurfaceKHR failed with the following error: '{}'.",
+                          VulkanUtils::ResultString(result));
+            return false;
+        }
+
+        return true;
+    }
+
+    DynamicArray<const char*> GetRequiredExtensionNames()
+    {
+        auto requiredExtensions = DynamicArray<const char*>();
+        requiredExtensions.EmplaceBack("VK_KHR_xcb_surface");
+        return requiredExtensions;
+    }
+}  // namespace C3D::VulkanPlatform
+
+#endif
